8331d4dfb82737973c91b9ad9e181c0db149f388
SkyFire <admin@projectskyfire.org>
2012-01-26 21:33:52 -0600

    Yup. I'm lazy. Forgot to update the copyright notices. They are all uniform now. Alto this branch is no longer supported so. If it's broken your SOL!
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- src/server/shared/Dynamic/UnorderedMap.h
+++ src/server/shared/Dynamic/UnorderedMap.h
@@ -1,16 +1,14 @@
 /*
- * Copyright (C) 2005-2011 MaNGOS <http://www.getmangos.com/>
- *
- * Copyright (C) 2008-2011 Trinity <http://www.trinitycore.org/>
- *
- * Copyright (C) 2010-2011 Project SkyFire <http://www.projectskyfire.org/>
+ * Copyright (C) 2010-2012 Project SkyFire <http://www.projectskyfire.org/>
+ * Copyright (C) 2005-2012 MaNGOS <http://www.getmangos.com/>
+ * Copyright (C) 2008-2012 Trinity <http://www.trinitycore.org/>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, 
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
@@ -19,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
-
+ 
 #ifndef TRINITY_UNORDERED_MAP_H
 #define TRINITY_UNORDERED_MAP_H
 
